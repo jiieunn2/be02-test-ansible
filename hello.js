e7ea5f79fc64d7d440c902bfbaaa32a2acec8057
kje <kimjieun121314@naver.com>
2024-02-19 15:28:01 +0900

    [Fix] js 파일 삭제

--- hello.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var content = function(req, resp) {
-	resp.end("Hello kje! 8000" + "\n");
-	resp.writeHead(200);
-}
-var w = http.createServer(content);
-w.listen(8000);
